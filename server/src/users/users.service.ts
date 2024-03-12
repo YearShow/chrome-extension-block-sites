598c08f197bbdbf424a8d51d2fcebc0fabf24d5f
Alexander Ershov <yearshow@yandex.ru>
2024-03-13 00:01:57 +0300

    Block list module

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { DbService } from 'src/db/db.service';
 import { AccountService } from 'src/account/account.service';
+import { BlockListService } from 'src/block-list/block-list.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     private db: DbService,
     private accountService: AccountService,
+    private blockListService: BlockListService,
   ) {}
 
   findByEmail(email: string) {
@@ -16,6 +18,7 @@ export class UsersService {
   async create(email: string, hash: string, salt: string) {
     const user = await this.db.user.create({ data: { email, hash, salt } });
     await this.accountService.create(user.id);
+    await this.blockListService.create(user.id);
 
     return user;
   }
