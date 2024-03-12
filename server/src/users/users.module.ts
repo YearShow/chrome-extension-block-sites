85bd341f66e6091205a47800a0b1670e0441aec3
Alexander Ershov <yearshow@yandex.ru>
2024-03-12 21:44:06 +0300

    Account module

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import {DbModule} from "src/db/db.module";
+import { DbModule } from 'src/db/db.module';
+import { AccountModule } from 'src/account/account.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, AccountModule],
   providers: [UsersService],
-  exports: [UsersService]
+  exports: [UsersService],
 })
 export class UsersModule {}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import {DbService} from "src/db/db.service";
+import { DbService } from 'src/db/db.service';
+import { AccountService } from 'src/account/account.service';
 
 @Injectable()
 export class UsersService {
-    constructor(private db: DbService) {}
+  constructor(
+    private db: DbService,
+    private accountService: AccountService,
+  ) {}
 
-    findByEmail(email: string) {
-        return this.db.user.findFirst({ where: { email }});
-    }
+  findByEmail(email: string) {
+    return this.db.user.findFirst({ where: { email } });
+  }
 
-    async create(email: string, hash: string, salt: string) {
-        console.log(email, hash, salt)
-        return this.db.user.create({ data: { email, hash, salt }});
-    }
+  async create(email: string, hash: string, salt: string) {
+    const user = await this.db.user.create({ data: { email, hash, salt } });
+    await this.accountService.create(user.id);
+
+    return user;
+  }
 }
-
